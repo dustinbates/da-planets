fd04eac1693347e0c20fecc5811859b6c88545a4
Dustin Bates <dustin.bates98@gmail.com>
2023-02-15 15:31:35 -0700

    server spins up

---
+++ client/app/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'codeworks-classroom.us.auth0.com'
+export const audience = 'https://codeworks-classroom.com'
+export const clientId = 'vag5QXyI2zuu0wjCwXSUUFPg0mzGVTNV'
